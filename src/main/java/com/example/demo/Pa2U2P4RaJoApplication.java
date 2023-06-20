d0addeec3ca252af3c0c9fcd50219af5e0c667ca
Jimmy090110 <fabricioortega2011@gmail.com>
2023-06-20 11:55:57 -0500

    Tarea6

--- src/main/java/com/example/demo/Pa2U2P4RaJoApplication.java
+++ src/main/java/com/example/demo/Pa2U2P4RaJoApplication.java
@@ -2,36 +2,37 @@ package com.example.demo;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.demo.repository.modelo.Autor;
 import com.example.demo.repository.modelo.Ciudadano;
 import com.example.demo.repository.modelo.Empleado;
 import com.example.demo.repository.modelo.Habitacion;
 import com.example.demo.repository.modelo.Hotel;
+import com.example.demo.repository.modelo.Libro;
+import com.example.demo.service.AutorService;
 import com.example.demo.service.CiudadanoService;
 import com.example.demo.service.EmpleadoService;
 import com.example.demo.service.HabitacionService;
 import com.example.demo.service.HotelService;
+import com.example.demo.service.LibroService;
 
 @SpringBootApplication
 public class Pa2U2P4RaJoApplication implements CommandLineRunner {
 	
-
-	@Autowired 
-	private CiudadanoService ciudadanoService;
+	@Autowired
+	private LibroService libroService;
 	
 	@Autowired
-	private EmpleadoService empleadoService;
-	
-	@Autowired
-	private HabitacionService habitacionService;
-	
-	@Autowired
-	private HotelService hotelService;
+	private AutorService autorService;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Pa2U2P4RaJoApplication.class, args);
@@ -40,22 +41,45 @@ public class Pa2U2P4RaJoApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
-		Hotel hote = new Hotel();
-		hote.setDireccion("Carapungo");
-		hote.setNombre("Hotel");
 		
-		Habitacion habi = new Habitacion();
-		habi.setHotel(hote);
-		habi.setNumero("1b");
-		habi.setValor(new BigDecimal(150));
 		
-		this.habitacionService.crear(habi);
+		Set<Libro>listaBib = new HashSet<>();
+	
+		Autor aut1 = new Autor();
+
+		Libro lib = new Libro();
+		Libro lib2 = new Libro();
+
+		lib.setEditorial("Frank");
+		lib.setTitulo("Animales pedidos");
 		
-		this.habitacionService.modificar(habi);
+		lib2.setTitulo("La Odisea");
+		lib2.setEditorial("Fernandez");
 		
-		this.habitacionService.buscar(1);
 		
-		this.habitacionService.eliminar(1);
+		aut1.setNombre("Pedro");
+		aut1.setApellido("Quintanilla");
+		listaBib.add(lib);
+		listaBib.add(lib2);
+		aut1.setLibros(listaBib);
+		
+		this.autorService.agregar(aut1);
+		
+		this.autorService.actualizar(aut1);
+		
+		this.autorService.buscar(1);
+		
+		this.autorService.eliminar(1);
+		
+		
+		
+
+		
+	
+		
+	
+		
+		
 		
 	
 	
